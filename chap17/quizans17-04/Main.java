984f6a3f1ccf3198df71cfcd361bb1b3e9cf5b49
Takuma SUGAWARA <sugawara@miyabilink.jp>
2021-09-16 16:42:15 +0900

    17章追加

---
+++ chap17/quizans17-04/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    System.out.println("プログラムが起動しました");
+    throw new IOException();
+  }
+}
