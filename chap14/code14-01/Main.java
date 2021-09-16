b3d9a5ede4ac443df582c028061989eeb2a0054d
Takuma SUGAWARA <sugawara@miyabilink.jp>
2021-09-16 16:03:47 +0900

    14章の追加

---
+++ chap14/code14-01/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    Empty e = new Empty();
+    String s = e.toString();
+    System.out.println(s);
+  }
+}
---
+++ chap14/code14-10/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    Hero h1 = new Hero();
+    Hero h2 = new Hero();
+    System.out.println(h1.money);
+    System.out.println(Hero.money);
+    /* … */
+  }
+}
