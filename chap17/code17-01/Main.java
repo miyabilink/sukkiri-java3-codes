92f619e3622b88721f6daf8a008be7ec6e5902ef
Takuma SUGAWARA <sugawara@miyabilink.jp>
2021-09-16 16:42:15 +0900

    17章追加

---
+++ chap17/code17-01/Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+    FileWriter fw = new FileWriter("data.txt");
+    /* : */
+  }
+}
---
+++ chap17/code17-05/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) {
+    try {
+      FileWriter fw = new FileWriter("data.txt");
+      fw.write("hello!");
+    } catch (Exception e) {
+      System.out.println("何らかの例外が発生しました");
+    } finally {
+      fw.close();
+    }
+  }
+}
