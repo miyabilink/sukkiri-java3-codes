b197cfffc77deafa2d0656d10abdca782295a684
Takuma SUGAWARA <sugawara@miyabilink.jp>
2021-11-04 13:49:17 +0900

    練習9-2 処理順を修正

--- chap09/quiz09-02/Main.java
+++ chap09/quiz09-02/Main.java
@@ -8,8 +8,8 @@ public class Main {
   public static void main(String[] args) {
     int baseHp = 25;
     Thief t = new Thief("アサカ", baseHp);
-    System.out.println(baseHp + " : " + t.hp);
     heal(baseHp);
+    System.out.println(baseHp + " : " + t.hp);
     heal(t);
     System.out.println(baseHp + " : " + t.hp);
   }
