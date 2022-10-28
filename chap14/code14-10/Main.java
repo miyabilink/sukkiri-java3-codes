af338cc6b0d18adee79aef790129179bb17fb4ce
Takuma SUGAWARA <sugawara@miyabilink.jp>
2022-10-28 09:37:30 +0900

    コード14-10 修正

--- chap14/code14-10/Main.java
+++ chap14/code14-10/Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     Hero h1 = new Hero();
     Hero h2 = new Hero();
-    System.out.println(h1.money);
+    System.out.println(h1.hp);
     System.out.println(Hero.money);
     /* … */
   }
